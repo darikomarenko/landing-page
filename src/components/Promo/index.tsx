98bef4d65bff0f78967ddc68e6487e4132043a66
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2024-03-28 18:01:26 +0300

    add images to table, add styles

--- src/components/Promo/index.tsx
+++ src/components/Promo/index.tsx
@@ -1,5 +1,7 @@
 import styles from './styles.module.scss'
 import cardOne from '../../assets/one.png'
+import workerOne from '../../assets/worker1.png'
+import workerTwo from '../../assets/worker2.png'
 
 export default function Promo() {
   return (
@@ -22,11 +24,14 @@ export default function Promo() {
         <div className={styles['promo__team__description-title']}>НЕМНОГО О НАШЕЙ КОМАНДЕ</div>
         <div className={styles['promo__team__description-text']}>Наша команда по верстке сайтов на тильде - это профессионалы, готовые создать для вас уникальные и современные веб-страницы. Мы обладаем глубокими знаниями в области веб-дизайна и разработки, что позволяет нам создавать сайты, которые не только эстетичны, но и функциональны.</div>
         </div>
-        <div className={styles['promo__team__table']}>
-        <div className={styles['promo__team__table-item']}>Alex</div>
-    <div className={styles['promo__team__table-item']}>Sofia</div>
-    <div className={styles['promo__team__table-item']}>Designer</div>
-    <div className={styles['promo__team__table-item']}>Market</div>
+    <div className={styles['promo__team__table']}>
+    <div className={styles['promo__team__table-item']}>МИРОНОВ АЛЕКСЕЙ</div>
+    <div className={styles['promo__team__table-item']}>КУЗНЕЦОВА СОФИЯ</div>
+    <div className={styles['promo__team__table-item']}>
+        <img src={workerOne} alt='photo'></img>
+        <div>ДИЗАЙНЕР</div></div>
+    <div className={styles['promo__team__table-item']}>
+    <img src={workerTwo} alt='photo'></img><div>МАРКЕТОЛОГ</div></div>
     </div>
         </div>
     </div>
