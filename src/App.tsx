7acb39fcb52f541b4729ab0d7d3c04a4f037f7f6
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2024-03-29 11:11:55 +0300

    add Sites components styles and jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Promo from '../src/components/Promo/index'
+import Sites from '../src/components/Sites/index'
 
 function App() {
 
   return (
     <>
     <Promo/>
+    <Sites/>
     </>
   )
 }
