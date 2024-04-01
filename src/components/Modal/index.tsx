13865258f6397500431c038ac280921e775a1973
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2024-04-01 17:52:52 +0300

    improve Form styles

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -7,9 +7,12 @@ interface ModalProps {
 }
 
 export default function Modal({ open, onClose }: ModalProps) {
+
+
   if (!open) {
     return null;
   }
+
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -27,20 +30,30 @@ export default function Modal({ open, onClose }: ModalProps) {
   return (
     <>
       <ModalBackground zindex="900" onClick={onClose}>
-        <div className={styles['popup']}>
-          <form className={styles['popup__form']} onSubmit={handleSubmit}>
-            <div className={styles['popup__title']}>Обсудить проект</div>
-            <div className={styles['popup__description']}>Оставьте свои контактные данные для обсуждения вашего проекта</div>
-            <div className={styles['popup__inputGroup']}>
-              <input type='text' className={styles['popup__input']} placeholder="Имя" name='name'/>
+        <div className={styles.popup}>
+          <form className={styles.popup__form} onSubmit={handleSubmit}>
+            <div className={styles.popup__title}>Обсудить проект</div>
+            <div className={styles.popup__description}>Оставьте свои контактные данные для обсуждения вашего проекта</div>
+            <div className={styles.popup__input}>
+              <div className={styles['popup__input__inputGroup']}>
+                <div className={styles['popup__input__inputGroup-one']}>
+                  <label>Имя</label>
+                  <input type='text' className={styles.popup__input} placeholder="Имя" name='name' required/>
+                  <hr/>
+                </div>
+                <div className={styles['popup__input__inputGroup-one']}>
+                  <label>Телефон</label>
+                  <input type='tel' className={styles.popup__input} placeholder="Телефон" name='phone' required/>
+                  <hr/>
+                </div>
+              </div>
+              <div className={styles.popup__input__inputGroupTwo}>
+                <label>Ваши пожелания по сайту</label>
+                <input type='text' className={styles.popup__input} name='wishes' />
+                <hr/>
+              </div>
             </div>
-            <div className={styles['popup__inputGroup']}>
-              <input type='tel' className={styles['popup__input']} placeholder="Телефон" name='phone'/>
-            </div>
-            <div className={styles['popup__inputGroup']}>
-              <input type='text' className={styles['popup__input']} placeholder="Пожелания" name='wishes' />
-            </div>
-            <button type="submit" className={styles['popup__button']}>Оставить заявку</button>
+            <button type="submit" className={styles.popup__button}>Оставить заявку</button>
           </form>
         </div>
       </ModalBackground>
