091f782b2f6a1c9257e4c40e833aa532110e31af
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2024-04-02 12:31:47 +0300

    test another way to specify image url

--- src/constants/Reviews/index.ts
+++ src/constants/Reviews/index.ts
@@ -11,7 +11,7 @@ export const data: IReviewItem[] = [
 	},
 	{
 		id: '677e2138-c1a4-4855-aa73-8fae4d062840',
-		photo:  'public/images/personTwo.png',
+		photo:  './landing-page/images/personTwo.png',
 		description:
 			'Спасибо команде за их услуги! Мой сайт выглядит стильно и профессионально благодаря им',
             hasButton: false,
@@ -19,33 +19,33 @@ export const data: IReviewItem[] = [
 	},
 	{
 		id: '3142cdd2-1ed8-48bd-9f6f-8206e93f50ac',
-		photo: 'public/images/personThree.png',
+		photo: './landing-page/images/personThree.png',
 		description: 'Работа с этими ребятами была невероятно продуктивной. Они быстро поняли мои потребности и создали сайт, который превзошел все ожидания',
         hasButton: false,
 	},
 	{
 		id: 'fbb80e01-360d-4b01-bd74-b91c321706d1',
-		photo: 'public/images/personFour.png',
+		photo: './landing-page/images/personFour.png',
 		description:
 			'Сотрудничество с этими дизайнерами - настоящее удовольствие! Они воплотили мои идеи в жизнь и создали сайт, о котором я мечтала',
             hasButton: false,
 	},
 	{
 		id: 'add057e7-cb49-440e-92d9-489740e65691',
-		photo: 'public/images/personFive.png',
+		photo: './landing-page/images/personFive.png',
 		description:
 			'Дизайнеры этого сайта - настоящие мастера своего дела! Их профессионализм и креативность помогли моему сайту выделиться среди конкурентов',
             hasButton: false,
 	},
 	{
 		id: '28487f63-6f96-4602-bf81-70c51d9ff8c6',
-		photo: 'public/images/personSix.png',
+		photo: './landing-page/images/personSix.png',
 		description: 'Сотрудничество с этими дизайнерами было отличным опытом! Они создали уникальный дизайн для моего сайта. А еще у них очень крутой сайт, какой гений это делал?',
             hasButton: false,
 	},
     {
 		id: '28487f63-6f96-4602-bf81-70c51d9ff8c0',
-		photo: 'public/images/personSeven.png',
+		photo: './landing-page/images/personSeven.png',
 		description:'Спасибо дизайнеру за креативный подход к созданию моего сайта! Они проявили талант и внимание к деталям',
         hasButton: false,
 	},
