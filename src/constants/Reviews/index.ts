466371862b3606d4cf2f02423cc15beb37574177
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2024-04-02 12:36:00 +0300

    test another way to specify image url

--- src/constants/Reviews/index.ts
+++ src/constants/Reviews/index.ts
@@ -3,7 +3,7 @@ import { IReviewItem } from './types';
 export const data: IReviewItem[] = [
 	{
 		id: '0d00ca1b-8a46-4377-bd55-f56563b4162e',
-		photo: './landing-page/personOne.png',
+		photo: './personOne.png',
 		description:
 			'Эти ребята просто удивительные! Я смогла создать свою страничку без единой проблемы. Рекомендую всем!',
         hasButton: false,
@@ -11,7 +11,7 @@ export const data: IReviewItem[] = [
 	},
 	{
 		id: '677e2138-c1a4-4855-aa73-8fae4d062840',
-		photo:  './landing-page/personTwo.png',
+		photo:  '.personTwo.png',
 		description:
 			'Спасибо команде за их услуги! Мой сайт выглядит стильно и профессионально благодаря им',
             hasButton: false,
@@ -19,33 +19,33 @@ export const data: IReviewItem[] = [
 	},
 	{
 		id: '3142cdd2-1ed8-48bd-9f6f-8206e93f50ac',
-		photo: './landing-page/personThree.png',
+		photo: '.personThree.png',
 		description: 'Работа с этими ребятами была невероятно продуктивной. Они быстро поняли мои потребности и создали сайт, который превзошел все ожидания',
         hasButton: false,
 	},
 	{
 		id: 'fbb80e01-360d-4b01-bd74-b91c321706d1',
-		photo: './landing-page/personFour.png',
+		photo: './personFour.png',
 		description:
 			'Сотрудничество с этими дизайнерами - настоящее удовольствие! Они воплотили мои идеи в жизнь и создали сайт, о котором я мечтала',
             hasButton: false,
 	},
 	{
 		id: 'add057e7-cb49-440e-92d9-489740e65691',
-		photo: './landing-page/personFive.png',
+		photo: './personFive.png',
 		description:
 			'Дизайнеры этого сайта - настоящие мастера своего дела! Их профессионализм и креативность помогли моему сайту выделиться среди конкурентов',
             hasButton: false,
 	},
 	{
 		id: '28487f63-6f96-4602-bf81-70c51d9ff8c6',
-		photo: './landing-page/personSix.png',
+		photo: './personSix.png',
 		description: 'Сотрудничество с этими дизайнерами было отличным опытом! Они создали уникальный дизайн для моего сайта. А еще у них очень крутой сайт, какой гений это делал?',
             hasButton: false,
 	},
     {
 		id: '28487f63-6f96-4602-bf81-70c51d9ff8c0',
-		photo: './landing-page/personSeven.png',
+		photo: './personSeven.png',
 		description:'Спасибо дизайнеру за креативный подход к созданию моего сайта! Они проявили талант и внимание к деталям',
         hasButton: false,
 	},
